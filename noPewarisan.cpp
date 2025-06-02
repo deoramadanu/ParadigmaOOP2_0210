422b27187d4d249c58f2232557457dae30b10091
deoramadanu <deo121005@gmail.com>
2025-06-02 18:44:20 +0700

    memanggil variable dan variable yang dipanggil

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -2,3 +2,27 @@
 using namespace std;
 
 class baseClass final
+{
+    public:
+        virtual void perkenalan()
+        {
+            cout << "Hallo saya function dari base class";
+        }
+};
+
+class deriveClass : public baseClass 
+{
+    public:
+        void perkenalan()
+        {
+            cout << "Hallo saya function dari derive Class";
+        }
+};
+
+int main()
+{
+    deriveClass a;
+    a.perkenalan();
+
+    return 0;
+}
